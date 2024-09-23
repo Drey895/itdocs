1e83d4268229e170e45a52172e2e07322b36f6a0
Даня Сафин <danyasafin@icloud.com>
2024-09-23 22:20:07 +0300

    seperate queries in different folders

--- src/quieries/insertUser.js
+++
@@ -1,9 +0,0 @@
-'use server'
-import { sql } from "@/database";
-
-export async function insertUser(user) {
-	const query = await sql`insert into users ${sql(user, 'username', 'password', 'role')}
-	returning *`
-	console.log(query)
-	return query
-}
--- src/quieries/selectUser.js
+++
@@ -1,9 +0,0 @@
-'use server'
-import { sql } from "@/database";
-
-export async function selectUser(username) {
-	const query = await sql`
-		SELECT * FROM users WHERE username = ${username}`
-	console.log(query)
-	return query
-}
