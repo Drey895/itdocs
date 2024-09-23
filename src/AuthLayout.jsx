23f29b48f081ee8a66fc9b61e24e02e2d9a100a8
Даня Сафин <danyasafin@icloud.com>
2024-09-23 22:14:12 +0300

    clean code

--- src/AuthLayout.jsx
+++ src/AuthLayout.jsx
@@ -1,27 +1,23 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from "react"
-import AuthForm from "./AuthForm"
-import RegForm from "./RegForm"
+import { useEffect, useState } from "react";
+import { AuthForm, RegForm } from "./forms";
 
-export function AuthLayout({children}) {
-	const [current, setCurrent] = useState(true)
-	const s = () => {
-		setCurrent((c) => !c)
-	}
+export function AuthLayout({ children }) {
+  const [current, setCurrent] = useState(true);
+  const s = () => {
+    setCurrent((c) => !c);
+  };
 
-	const [isRendered, setIsRendered] = useState(false);
-	const [isAuth, setIsAuth] = useState(false);
+  const [isRendered, setIsRendered] = useState(false);
+  const [isAuth, setIsAuth] = useState(false);
 
-	useEffect(() => {
-		if (localStorage.getItem('user'))
-			setIsAuth(true)
-		setIsRendered(true)
-	}, []);
+  useEffect(() => {
+    if (localStorage.getItem("user")) setIsAuth(true);
+    setIsRendered(true);
+  }, []);
 
-	if (!isRendered)
-		return <></>
-	if (isAuth)
-		return children
-	return current ? <AuthForm sw={s}/> : <RegForm sw={s}/>
+  if (!isRendered) return <></>;
+  if (isAuth) return children;
+  return current ? <AuthForm sw={s} /> : <RegForm sw={s} />;
 }
---
+++ src/forms/AuthForm.jsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useRef } from "react";
+import { useFormState } from "react-dom";
+import { useForm } from "react-hook-form";
+import { Input, Button } from "@/components";
+import { loginAction } from "@/actions/loginAction";
+
+export function AuthForm({ sw }) {
+  const [state, action] = useFormState(loginAction, null);
+  const { register } = useForm();
+
+  if (state?.data) {
+    localStorage.setItem("user", JSON.stringify(state.data));
+    window.location.reload();
+  }
+
+  const pwdRef = useRef(null);
+  const { ref, ...rest } = register("password");
+
+  return (
+    <div className="flex flex-col gap-6 font-mono">
+      <h2 className="text-2xl text-center">Система авторизации</h2>
+      <form action={action} className={"flex flex-col gap-4"}>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-0.5">
+            <label htmlFor="username">Логин</label>
+            <Input s={{ ...register("username"), required: true }} />
+          </div>
+          <div className="flex flex-col gap-0.5">
+            <label htmlFor="password">Пароль</label>
+            <Input
+              s={{ ...rest, ref: pwdRef, type: "password", required: true }}
+            />
+          </div>
+          {state?.error !== null && (
+            <div className="text-red-500">{state?.error}</div>
+          )}
+        </div>
+        <div className="flex flex-col gap-2.5">
+          <Button className="text-white bg-blue-500 hover:bg-blue-600">
+            Войти
+          </Button>
+          <Button
+            className="bg-white hover:bg-black/5"
+            type="button"
+            onClick={sw}
+          >
+            Регистрация
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default AuthForm;
