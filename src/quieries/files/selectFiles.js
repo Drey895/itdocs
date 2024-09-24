0576a3a9e8e7d25e808908afcb91513811997e81
Даня Сафин <danyasafin@icloud.com>
2024-09-24 13:55:35 +0300

    update function for selectFiles

--- src/quieries/files/selectFiles.js
+++ src/quieries/files/selectFiles.js
@@ -2,7 +2,7 @@
 
 import { sql } from "@/database";
 
-export async function selectFiles(userId, cursor, limit) {
+export async function selectFiles(userId, limit, cursor = 0) {
   const query =
     await sql`SELECT * FROM files WHERE id > ${cursor} AND user_id = ${userId} ORDER BY created_at DESC, id LIMIT ${limit}`;
   console.log(query);
