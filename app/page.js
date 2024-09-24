0e46a04f066dc0e52fc1c7368f45cc9529aa8f49
Даня Сафин <danyasafin@icloud.com>
2024-09-24 13:41:40 +0300

    add select query for files

--- app/page.js
+++ app/page.js
@@ -1,14 +1,9 @@
-import { Home } from "@/home";
 import { AuthLayout } from "@/AuthLayout";
 
-export default function Home() {
+export default function Page() {
   return (
-    <main className="w-full h-[100vh] flex flex-col justify-center items-center gap-2">
-      <img src="/logo.png" alt="" />
-      <AuthLayout>
-        <title>Добро пожаловать! | IT-Docs</title>
-        <Home/>
-      </AuthLayout>
-    </main>
+    <AuthLayout>
+      <main></main>
+    </AuthLayout>
   );
 }
