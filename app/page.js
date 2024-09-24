900aa87863568c6ca268432123de7d8ad963e2d2
Даня Сафин <danyasafin@icloud.com>
2024-09-24 14:04:43 +0300

    deleted some images and modified some files

--- app/page.js
+++ app/page.js
@@ -2,12 +2,9 @@ import { Home } from "@/Home";
 import { AuthLayout } from "@/AuthLayout";
 
 export default function Page() {
-export default function Page() {
   return (
-    <main className="w-full h-[100vh] flex flex-col justify-center items-center gap-2">
-      <AuthLayout>
-        <Home />
-      </AuthLayout>
-    </main>
+    <AuthLayout>
+      <Home />
+    </AuthLayout>
   );
 }
