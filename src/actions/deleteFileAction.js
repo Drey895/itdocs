b85332734b50a64717bcc66577943f87a70b37ef
Даня Сафин <danyasafin@icloud.com>
2024-09-28 14:47:34 +0300

    Refactor deleteFileAction to remove file from disk after deletion

--- src/actions/deleteFileAction.js
+++ src/actions/deleteFileAction.js
@@ -1,11 +1,17 @@
+"use server";
+
 import { selectFileById } from "@/queries/files";
 import { deleteFile } from "@/queries/files/deleteFile";
 import { getUser } from "@/user";
+import path from "path";
+import fs from "fs/promises";
 
 export async function deleteFileAction(fileId) {
   const user = await getUser();
   const file = (await selectFileById(fileId))[0] ?? null;
   if (user && file && (user.role === "admin" || user.id === file.user_id)) {
-    deleteFile(fileId);
+    const filePath = path.join("files", file.type, String(fileId));
+    await deleteFile(fileId);
+    await fs.rm(filePath);
   }
 }
